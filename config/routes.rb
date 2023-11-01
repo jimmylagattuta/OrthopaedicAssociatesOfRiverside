36c34b19c2fcd39f4a40b5fe02ccb631ba5f89d3
James Lagattuta <jimmy.lagattuta@gmail.com>
2023-11-01 02:43:00 -0700

    wirup

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :notes, only: [:index]
 
-      get "/me", to: "users#show"
+
       post "/signup", to: "users#create"
       post "/login", to: "sessions#create"
       delete "/logout", to: "sessions#destroy"
