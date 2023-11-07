ac029bab5d18de74a3095c978f46169df5089c35
James Lagattuta <jimmy.lagattuta@gmail.com>
2023-11-06 23:18:49 -0800

    yelp search

--- app/controllers/api/v1/jobs_controller.rb
+++ app/controllers/api/v1/jobs_controller.rb
@@ -45,7 +45,8 @@ class Api::V1::JobsController < ApplicationController
       http = Net::HTTP.new("api.yelp.com", 443)
       http.use_ssl = true
 
-      url = URI("https://api.yelp.com/v3/businesses/search?term=#{URI.encode(search_term)}")
+      encoded_search_term = URI::Escape.encode(search_term) # Use URI::Escape.encode
+      url = URI("https://api.yelp.com/v3/businesses/search?term=#{encoded_search_term}")
       request = Net::HTTP::Get.new(url)
       request["Accept"] = 'application/json'
       request["Authorization"] = "Bearer #{ENV['REACT_APP_YELP_API_KEY']}"
@@ -75,7 +76,6 @@ class Api::V1::JobsController < ApplicationController
     end
   end
 end
-
 
 
 
