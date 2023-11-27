b05da0dd3242d9e2f9e5e502926a5af0b57a6479
James Lagattuta <jimmy.lagattuta@gmail.com>
2023-11-27 12:33:38 -0800

    updated about us component

--- client/src/components/AboutUsComponent.js
+++ client/src/components/AboutUsComponent.js
@@ -22,9 +22,7 @@ const AboutUsComponent = () => {
                 <p className='about-description-right'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis placerat odio, id tincidunt justo dictum eget. Nunc tincidunt, quam id volutpat dictum, purus odio laoreet tellus, quis tristique odio nisi eget ex. Sed luctus vel ipsum sit amet bibendum. Nullam lacinia tellus a est tincidunt, vel bibendum purus gravida. Fusce feugiat dui nec justo mattis, ut placerat urna gravida. Morbi et nunc eu urna volutpat suscipit. Aenean efficitur eros vel nisi malesuada venenatis. Donec et volutpat libero. Suspendisse feugiat justo non erat bibendum, eget viverra dolor pharetra.
                 </p>
-                <p className='about-description-right'>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sagittis placerat odio, id tincidunt justo dictum eget. Nunc tincidunt, quam id volutpat dictum, purus odio laoreet tellus, quis tristique odio nisi eget ex. Sed luctus vel ipsum sit amet bibendum. Nullam lacinia tellus a est tincidunt, vel bibendum purus gravida. Fusce feugiat dui nec justo mattis, ut placerat urna gravida. Morbi et nunc eu urna volutpat suscipit. Aenean efficitur eros vel nisi malesuada venenatis. Donec et volutpat libero. Suspendisse feugiat justo non erat bibendum, eget viverra dolor pharetra.
-                </p>
+                
                 <div className='about-right-button'>
                     <NavLink className='btn header-button-white' to='/about'>
                         Learn More
